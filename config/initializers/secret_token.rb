323b4ad4e131312903965d2927de981eaa7dc86b
Jarett Sims <jarett.sims@gmail.com>
2013-12-11 22:49:32 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NYSMIConcussion::Application.config.secret_key_base = '145548a836c29d3be4957fc557ed61085b63e9bf484b5fd39a02c0a5cd23bf1b80b20d6232d0e1736b1e1b6ae6a4b3f093000a2b3834036ea90fbc26c95224c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NYSMIConcussion::Application.config.session_store :cookie_store, key: '_NYSMI_concussion_session'
